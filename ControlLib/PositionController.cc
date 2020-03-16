5bf60effaaf800a969066c573f996798146bc495
Bill Fan <fanbenlong@gmail.com@qq.com>
2020-03-16 19:19:13 -0400

    Separated out pitch and roll position control functions

--- ControlLib/PositionController.cc
+++ ControlLib/PositionController.cc
@@ -1,6 +1,7 @@
 #include "include/PositionController.hh"
 
 PositionController::PositionController()
+    : pid_altitude(0.13, 0.05, 0, 0, -1, 1, -1, 1) // PID controller initialization
 {
 }
 
@@ -8,7 +9,7 @@ PositionController::~PositionController()
 {
 }
 
-void PositionController::updateSensors(SensorData &_sensorData)
+void PositionController::updateSensors(PositionController::SensorData &_sensorData)
 {
     this->lastPitch = _sensorData.pitch;
     this->lastRoll = _sensorData.roll;
@@ -16,13 +17,30 @@ void PositionController::updateSensors(SensorData &_sensorData)
     this->lastAltitude = _sensorData.range * std::cos(this->lastPitch) * std::cos(this->lastRoll);
 }
 
-void PositionController::updateUserSettings(UserSettings &_userSettings)
+void PositionController::updateUserSettings(PositionController::UserSettings &_userSettings)
 {
-    this->lastRollSP = _userSettings.roll;
+    this->lastRollSP = _userSettings.rollSP;
     this->lastAltitudeSP = _userSettings.altitude;
 }
 
-AttitudeController::AttitudeSP PositionController::controlPosition()
+AttitudeController::AttitudeSP PositionController::controlPosition(double dt)
 {
+    AttitudeController::AttitudeSP out;
+    out.pitchSP = getPitchSP(dt);
+    out.rollSP = getRollSP();
 
+    return out;
+}
+
+double PositionController::getPitchSP(double dt)
+{
+    float error, pitchSP;
+    error = lastAltitudeSP - lastAltitude;
+    pitchSP = pid_altitude.update(lastAltitudeSP, error, dt);
+    return pitchSP;
+}
+
+double PositionController::getRollSP()
+{
+    return this->lastRollSP;
 }
--- ControlLib/include/PositionController.hh
+++ ControlLib/include/PositionController.hh
@@ -1,6 +1,7 @@
 #include <cmath>
 
 #include "AttitudeController.hh"
+#include "PIDFF.hh"
 
 class PositionController
 {
@@ -10,18 +11,16 @@ public:
     {
         double range;
 
-        double pitch;
-        double roll;
+        double pitch; // assumes up is positive
+        double roll; // assumes cw is positive (looking from behind)
         // double yaw;
-
-        double acceleration_lateral;
     };
 
     // Struct containing user settings 
     struct UserSettings
     {
         double altitude;
-        double roll;
+        double rollSP;
     };
 
     PositionController();
@@ -30,14 +29,19 @@ public:
     void PositionController::updateSensors(SensorData &_sensorData);
     void PositionController::updateUserSettings(UserSettings &_userSettings);
 
-    AttitudeController::AttitudeSP PositionController::controlPosition();
+    AttitudeController::AttitudeSP PositionController::controlPosition(double dt);
+    double PositionController::getPitchSP(double dt);
+    double PositionController::getRollSP();
+
 
 private:
+
+    PIDFF pid_altitude;
+
     double lastAltitude{0.0};
     double lastPitch{0.0};
     double lastRoll{0.0};
     // double lastYaw{0.0};
-    double lastAccel_lateral{0.0};
 
     double lastAltitudeSP{0.0};
     double lastRollSP{0.0};
